ac32e0653374852a12d9a06fd6605068903722d5
Oli Le Maire <olilemaire85@gmail.com>
2020-12-01 21:17:55 +0000

    added simplecov in spec_helper.rb and refactored code

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,17 +2,13 @@ require 'bank'
 
 describe Bank do
   it 'lets a user see his/her bank balance' do
-    expect(subject.balance).to eq (0)
-  end
-
-  it 'lets a user deposit money into their bank balance' do
-    expect(subject.deposited(100)).to eq (100)
+    expect(subject.balance).to eq 0
   end
 
   it 'adds deposited money to the users bank balance' do
     subject.deposited(500)
     subject.add_deposit_to_balance
-    expect(subject.balance).to eq (500)
+    expect(subject.balance).to eq 500
   end
 
 end
