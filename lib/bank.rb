63b4e58de22daf3482656f0152c1f551d7f62df6
Oli Le Maire <olilemaire85@gmail.com>
2020-12-01 20:55:39 +0000

    add a passing test for a method which lets a user add money to their account balance

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,7 +1,15 @@
 class Bank
 
+  def initialize
+    @balance = 0
+  end
+
   def balance
-    0
+    @balance
+  end
+
+  def deposited(amount_to_deposit)
+    @amount_to_deposit = amount_to_deposit
   end
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,7 +1,14 @@
 require 'bank'
 
 describe Bank do
-  it 'lets the user see his/her bank balance' do
-    expect(subject.balance).to eq(0)
+  it 'lets a user see his/her bank balance' do
+    expect(subject.balance).to eq (0)
   end
+
+  it 'lets a user deposit money into their bank balance' do
+    expect(subject.deposited(100)).to eq (100)
+  end
+
+
+
 end
